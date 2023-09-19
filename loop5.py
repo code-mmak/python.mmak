81e2e451db8a7f7c22bd14669e800dac1f04a4be
code-mmak <144559994+code-mmak@users.noreply.github.com>
2023-09-19 12:45:34 +0500

    Update loop5.py

--- loop5.py
+++ loop5.py
@@ -14,3 +14,9 @@ for revrow in reversed(range(1,size)):
     for column in range(2*revrow-1):
        print("#", end="")
     print()
+for row in range(2,size+1):
+    for space in reversed(range(row, size)):
+        print(" ", end="")
+    for column in range(2*row-1):
+       print("#", end="")
+    print()
